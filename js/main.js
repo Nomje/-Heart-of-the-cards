98c5a1164d1a0cee8e3259b604bef1f69c04dd46
nomji <navjeetschattha@gmail.com>
2022-08-09 10:55:26 -0700

    audio broke

--- js/main.js
+++ js/main.js
@@ -1,47 +1,95 @@
-const audio = {
-        // assiagn the audio to buttons by giving the buttons name
-    //     laser: 'http://www.freesound.org/data/previews/42/42106_70164-lq.mp3',
-    //     dog: 'http://www.freesound.org/data/previews/327/327666_5632380-lq.mp3',
-    //     cow: 'http://www.freesound.org/data/previews/58/58277_634166-lq.mp3',
-    //     siren: 'http://www.freesound.org/data/previews/336/336899_4939433-lq.mp3'
-    }
-
-var sounds ={
-    clip1: Object.assign(document.createElement('audio'),{
-        src : '',
-    }),
-    clip2: Object.assign(document.createElement('audio'),{
-        src : '',
-    }),
-     clip3: Object.assign(document.createElement('audio'),{
-        src : '',
-    }),
-    clip4: Object.assign(document.createElement('audio'),{
-        src : '',
-    }),
-    clip5: Object.assign(document.createElement('audio'),{
-        src : '',
-    }),
-    clip6: Object.assign(document.createElement('audio'),{
-        src : '',
-    }),
-}
+alert("is this working");
 
 
-    let pairs = 0;
-    let audio1, audio2;
-    let turns = 0;
 
-    function pairFound (audio1, audio2){
-        if (audio1 === audio2){
-            //change back ground to certian color and diable the dom events on said buttons 
-        }
-        else {
-            // change the background to bad color and do nothing 
-        }
-    }
 
-    function shuffleAudio (){
-        pair = 0;
-        e
-    }
+
+
+
+
+
+
+
+
+// const sounds = {
+//     pcFail: "soundFiles/bong.mp3",// audio pathing 
+//     lightSaber: "soundFiles/lightSaber.wav",
+//     dogBark: "soundFiles/bark.wav"
+//     wowSound: "soundFiles./wow.wav"
+//     fart: "soundFiles",
+//     trasformer,""
+// }
+
+
+// not working code // const audio = {
+// //         // assiagn the audio to buttons by giving the buttons name
+// //         bug: "soundFiles/critical-stop.mp3",
+// //         dog: 'http://www.freesound.org/data/previews/327/327666_5632380-lq.mp3',
+// //         cow: 'http://www.freesound.org/data/previews/58/58277_634166-lq.mp3',
+// //         siren: 'http://www.freesound.org/data/previews/336/336899_4939433-lq.mp3'
+// //     }
+// let turn = 0
+// const music = new Audio();
+// const bgPlayer = document.getElementById('bg-player');
+// document.getElementById('play-btn').addEventListener('click', handlePlaySound);
+
+
+// // function handlePlaySound() {
+// // const selSoundInp = document.querySelector('input[name="sound"]:checked');
+// // playSound(selSoundInp.value);
+// // }
+
+// // function playSound(name) {
+// //     music.src = sounds[name];
+// //     music.play();
+// //   }
+
+// //   function handleBgChanged() {
+// //     bgCheckbox.checked ? bgPlayer.play() : bgPlayer.pause();
+// //   }
+  
+// function turnCounter (){
+//     turn ++;
+//     document.getElementById("click").innerHTML = turn;
+// }
+
+// // var sounds ={
+// //     clip1: Object.assign(document.createElement('audio'),{
+// //         src : '',
+// //     }),
+// //     clip2: Object.assign(document.createElement('audio'),{
+// //         src : '',
+// //     }),
+// //      clip3: Object.assign(document.createElement('audio'),{
+// //         src : '',
+// //     }),
+// //     clip4: Object.assign(document.createElement('audio'),{
+// //         src : '',
+// //     }),
+// //     clip5: Object.assign(document.createElement('audio'),{
+// //         src : '',
+// //     }),
+// //     clip6: Object.assign(document.createElement('audio'),{
+// //         src : '',
+// //     }),
+// // }
+
+
+// //     let pairs = 0;
+// //     let audio1, audio2;
+// //     let turns = 0;
+
+// //     function pairFound (audio1, audio2){
+// //         if (audio1 === audio2){
+// //             //change back ground to certian color and diable the dom events on said buttons 
+// //         }
+// //         else {
+// //             // change the background to bad color and do nothing 
+// //         }
+// //     }
+
+// //     function shuffleAudio (){
+// //         pair = 0;
+// //         e
+// //     }
+
